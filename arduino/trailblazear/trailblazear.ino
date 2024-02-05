66abbe2d0d449250fac05722f51af10d887dba98
AL <alexander@levineportfolio.com>
2024-02-05 13:21:10 -0500

    IDE setup

---
+++ arduino/trailblazear/trailblazear.ino
@@ -0,0 +1,17 @@
+/*
+*
+*/
+
+//LIBRARIES
+#include <TensorFlowLite.h>
+#include <Arduino_LSM9DS1.h>
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
